54b33e8ca9286f8e58c09f2794c3d399f069079f
heavy-lifting <lucy.cheesman@gmail.com>
2026-06-05 21:46:32 +0100

    pasting resize fun in as import not working (can fix this later)

--- data_parsing/make_mobd.py
+++ data_parsing/make_mobd.py
@@ -3,10 +3,11 @@ import os
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
-from .utils import resize
+# from .utils import resize
 from pathlib import Path
 from functools import partial
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from scipy.interpolate import interp1d
 
 DEVICE_HZ = 100  # Hz
 WINDOW_SEC = 10  # seconds
@@ -25,6 +26,22 @@ datafolder = os.path.join("/mnt/parscratch/users/acp25lmc/joined_data_parquet")
 sites = ["MS21"]
 OUTDIR = os.path.join("..", "data", "mobd_clean")
 num_workers = 4  # update this based on number of cores requested
+
+def resize(X, length, axis=1):
+    """Resize the temporal length using linear interpolation.
+    X must be of shape (N,M,C) (channels last) or (N,C,M) (channels first),
+    where N is the batch size, M is the temporal length, and C is the number
+    of channels.
+    If X is channels-last, use axis=1 (default).
+    If X is channels-first, use axis=2.
+    """
+    length_orig = X.shape[axis]
+    t_orig = np.linspace(0, 1, length_orig, endpoint=True)
+    t_new = np.linspace(0, 1, length, endpoint=True)
+    X = interp1d(t_orig, X, kind="linear", axis=axis, assume_sorted=True)(
+        t_new
+    )
+    return X
 
 
 def is_good_quality(w):
