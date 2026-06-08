bf02db099809db12bb054ae10a0bb12209c1edea
heavy-lifting <lucy.cheesman@gmail.com>
2026-06-08 10:15:16 +0100

    Unified output path

--- slurm-jobs/job-make-mobd.sh
+++ slurm-jobs/job-make-mobd.sh
@@ -5,8 +5,7 @@
 #SBATCH --time=6:00:00
 #SBATCH --mail-user=lmcheesman1@sheffield.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.out
-#SBATCH --error=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.err
+#SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.log
 
 export SLURM_EXPORT_ENV=ALL
 module load Anaconda3/2024.02-1
