9a82a43d1aed70a6d5867322d2138f254a36e8f0
heavy-lifting <lucy.cheesman@gmail.com>
2026-06-12 11:38:41 +0100

    Created slurm job for sampled data

---
+++ slurm-jobs/job-train-downstream-sampled.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=TRAIN_DOWNSTREAM
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16000
+#SBATCH --time=6:00:00
+#SBATCH --mail-user=lmcheesman1@sheffield.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.log
+
+export SLURM_EXPORT_ENV=ALL
+module load Anaconda3/2024.02-1
+source activate ssl_env
+cd /users/acp25lmc/ssl-wearables/data_parsing
+python /users/acp25lmc/ssl-wearables/downstream_task_evaluation.py "data=mobd_10s_sampled" "evaluation=all" "evaluation.flip_net_path=/users/acp25lmc/ssl-wearables/model_check_point/mtl_best.mdl"
