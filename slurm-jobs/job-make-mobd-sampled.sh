c576b67b53c58579a089b4b473c94b631af1327d
heavy-lifting <lucy.cheesman@gmail.com>
2026-06-12 12:08:07 +0100

    Created slurm job for sampled data

---
+++ slurm-jobs/job-make-mobd-sampled.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=MAKE_MOBD
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16000
+#SBATCH --time=1:00:00
+#SBATCH --mail-user=lmcheesman1@sheffield.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.log
+
+export SLURM_EXPORT_ENV=ALL
+module load Anaconda3/2024.02-1
+source activate ssl_env
+cd /users/acp25lmc/ssl-wearables/data_parsing
+python -u make_mobd_sampled.py
