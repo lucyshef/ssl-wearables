8fb5195dd0f7202b9aeb261089058904a883e003
heavy-lifting <lucy.cheesman@gmail.com>
2026-06-12 23:45:58 +0100

    Fixed GPU job

--- slurm-jobs/job-train-downstream-sampled-gpu.sh
+++ slurm-jobs/job-train-downstream-sampled-gpu.sh
@@ -8,11 +8,10 @@
 #SBATCH --time=20:00:00
 #SBATCH --mail-user=lmcheesman1@sheffield.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/users/acp25lmc/predicting-nep-status/slurm-jobs/logs/%x_%j_%a.out
-#SBATCH --error=/users/acp25lmc/predicting-nep-status/slurm-jobs/logs/%x_%j_%a.err
+#SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.log
 
 module load Anaconda3/2024.02-1
 module load CUDA/12.4.0
-source activate env
+source activate ssl_env
 cd /users/acp25lmc/ssl-wearables/data_parsing
 python /users/acp25lmc/ssl-wearables/downstream_task_evaluation.py "data=mobd_10s_sampled" "evaluation=all" "evaluation.flip_net_path=/users/acp25lmc/ssl-wearables/model_check_point/mtl_best.mdl"
