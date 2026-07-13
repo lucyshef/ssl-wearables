ac16dc211492decd927bca3f84c0a5561afecc3f
heavy-lifting <lucy.cheesman@gmail.com>
2026-07-13 11:42:16 +0100

    sample across whole week (30hz, 100 windows)

--- data_parsing/make_mobd_sampled.py
+++ data_parsing/make_mobd_sampled.py
@@ -9,7 +9,7 @@ from functools import partial
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from scipy.interpolate import interp1d
 
-DEVICE_HZ = 100  # Hz
+DEVICE_HZ = 30  # Hz
 WINDOW_SEC = 10  # seconds
 WINDOW_OVERLAP_SEC = 0  # i'm doing no overlap as I probably don't really care about activity boundaries
 WINDOW_LEN = int(DEVICE_HZ * WINDOW_SEC)  # device ticks
@@ -21,7 +21,7 @@ TARGET_HZ = 100  # Hz
 TARGET_WINDOW_LEN = int(TARGET_HZ * WINDOW_SEC)
 # SUBSAMPLE_DURATION = 24
 SUBSAMPLE_DURATION = (24 * 7)
-MAX_WINDOWS=500
+MAX_WINDOWS=100
 
 
 # DATAFILES = "/Users/catong/repos/video-imu/data/"
