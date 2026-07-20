399c73ca68dc963a557c0ab9d1f1795e43d13bb8
heavy-lifting <lucy.cheesman@gmail.com>
2026-07-20 13:30:06 +0100

    increase job time for weekly data

--- slurm-jobs/job-make-mobd-sampled.sh
+++ slurm-jobs/job-make-mobd-sampled.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=MAKE_MOBD_SAMPLED
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=250G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mail-user=lmcheesman1@sheffield.ac.uk
 #SBATCH --mail-type=ALL
 #SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.log
