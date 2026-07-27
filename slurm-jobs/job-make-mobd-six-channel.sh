fef500f024b44fad5318f95de2188f5dadff65f5
heavy-lifting <lucy.cheesman@gmail.com>
2026-07-27 13:49:12 +0100

    prepping to sample six-channel on 10s, 30s and 60s windows

--- slurm-jobs/job-make-mobd-six-channel.sh
+++ slurm-jobs/job-make-mobd-six-channel.sh
@@ -2,13 +2,46 @@
 #SBATCH --job-name=MAKE_MOBD_SIX_CHANNEL
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=250G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mail-user=lmcheesman1@sheffield.ac.uk
 #SBATCH --mail-type=ALL
 #SBATCH --output=/users/acp25lmc/ssl-wearables/slurm-jobs/logs/%x_%j_%a.log
+#SBATCH --array=1-11
+
+# Define the parameter sets for each array task:
+# Format: "DEVICE_HZ = 100 (Hz)
+           #WINDOW_SEC = 10 (seconds)
+           #TARGET_HZ = 30  # Hz
+           #SUBSAMPLE_DURATION = 24 (hours)
+           #MAX_WINDOWS=100
+
+
+if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
+    PARAMS="100 10 30 24 500"
+elif [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
+    PARAMS="100 10 100 24 100"
+elif [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
+    PARAMS="100 10 100 24 500"
+elif [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
+    PARAMS="100 60 30 24 100"
+elif [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
+    PARAMS="100 60 30 24 500"
+elif [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
+    PARAMS="100 60 100 24 100"
+elif [ $SLURM_ARRAY_TASK_ID -eq 7 ]; then
+    PARAMS="100 60 100 24 500"
+elif [ $SLURM_ARRAY_TASK_ID -eq 8 ]; then
+    PARAMS="100 30 30 24 100"
+elif [ $SLURM_ARRAY_TASK_ID -eq 9 ]; then
+    PARAMS="100 30 30 24 500"
+elif [ $SLURM_ARRAY_TASK_ID -eq 10 ]; then
+    PARAMS="100 30 100 24 100"
+elif [ $SLURM_ARRAY_TASK_ID -eq 11 ]; then
+    PARAMS="100 30 100 24 500"
+fi
 
 export SLURM_EXPORT_ENV=ALL
 module load Anaconda3/2024.02-1
 source activate ssl_env
 cd /users/acp25lmc/ssl-wearables/data_parsing
-python -u make_mobd_six_channel.py
+python -u make_mobd_six_channel.py $PARAMS
